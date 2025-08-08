19a9d10d660dfb9eab79a9cfb07f1a2a595cc1ca
Quantaralive <info.quantara@gmail.com>
2025-08-08 11:30:03 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <div style={{ color: '#00f0ff', background: '#000', padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>QUANTARA</h1>
+      <p>Benvenuto! Stai visualizzando una demo futuristica.</p>
+    </div>
+  );
+}
